588698a7fd5af26d327c3dda02a07549dbd898dc
Ife <45706096+EngineeredWithIfe@users.noreply.github.com>
2024-07-18 08:57:09 -0400

    Update Form3.Designer.cs

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -1,91 +1,78 @@
-﻿namespace ACME_Web_App
+namespace ACME_Web_App
 {
     partial class PortalScreen
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            if (disposing && components != null)
             {
                 components.Dispose();
             }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.customerButton = new System.Windows.Forms.Button();
+            this.adminButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // orderButton
             // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(275, 59);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(220, 66);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Order Portal";
-            this.button1.UseVisualStyleBackColor = true;
+            this.orderButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.orderButton.Location = new System.Drawing.Point(275, 59);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(220, 66);
+            this.orderButton.TabIndex = 1;
+            this.orderButton.Text = "Order Portal";
+            this.orderButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // customerButton
             // 
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Location = new System.Drawing.Point(275, 155);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(221, 67);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Customer Portal";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.customerButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.customerButton.Location = new System.Drawing.Point(275, 155);
+            this.customerButton.Name = "customerButton";
+            this.customerButton.Size = new System.Drawing.Size(221, 67);
+            this.customerButton.TabIndex = 2;
+            this.customerButton.Text = "Customer Portal";
+            this.customerButton.UseVisualStyleBackColor = true;
+            this.customerButton.Click += new System.EventHandler(this.CustomerButton_Click);
             // 
-            // button3
+            // adminButton
             // 
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Location = new System.Drawing.Point(274, 247);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(220, 63);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Admin Portal ";
-            this.button3.UseVisualStyleBackColor = true;
+            this.adminButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.adminButton.Location = new System.Drawing.Point(274, 247);
+            this.adminButton.Name = "adminButton";
+            this.adminButton.Size = new System.Drawing.Size(220, 63);
+            this.adminButton.TabIndex = 3;
+            this.adminButton.Text = "Admin Portal";
+            this.adminButton.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // backButton
             // 
-            this.button4.BackgroundImage = global::ACME_Web_App.Properties.Resources.reallysmallbackarrow;
-            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.Location = new System.Drawing.Point(12, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(55, 32);
-            this.button4.TabIndex = 8;
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.backButton.BackgroundImage = global::ACME_Web_App.Properties.Resources.reallysmallbackarrow;
+            this.backButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.backButton.Location = new System.Drawing.Point(12, 12);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(55, 32);
+            this.backButton.TabIndex = 8;
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.BackButton_Click);
             // 
-            // pictureBox1
+            // logoPictureBox
             // 
-            this.pictureBox1.Image = global::ACME_Web_App.Properties.Resources.blurlogo1;
-            this.pictureBox1.Location = new System.Drawing.Point(-8, 345);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(335, 109);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.logoPictureBox.Image = global::ACME_Web_App.Properties.Resources.blurlogo1;
+            this.logoPictureBox.Location = new System.Drawing.Point(-8, 345);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(335, 109);
+            this.logoPictureBox.TabIndex = 0;
+            this.logoPictureBox.TabStop = false;
             // 
             // PortalScreen
             // 
@@ -93,24 +80,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.adminButton);
+            this.Controls.Add(this.customerButton);
+            this.Controls.Add(this.orderButton);
+            this.Controls.Add(this.logoPictureBox);
             this.Name = "PortalScreen";
             this.Text = "Portal Screen";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
             this.ResumeLayout(false);
-
         }
 
-        #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.Button customerButton;
+        private System.Windows.Forms.Button adminButton;
+        private System.Windows.Forms.Button backButton;
     }
 }
