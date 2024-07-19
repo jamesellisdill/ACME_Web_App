971144327823098f6c708b60870f0f5ce40d49a3
CoryAllenOfficial <coryallen92@gmail.com>
2024-07-19 14:13:39 -0400

    Updated Order.cs to include comments

--- Order.cs
+++ Order.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,26 +8,31 @@ namespace ACME_Web_App
 {
     public class Order
     {
+        // ATTRIBUTES
         private List<Product> order;
         private static int orderIDCounter = 6000000;
         private int orderID;
 
+        // CONSTRUCTOR
         public Order()
         {
             orderID = orderIDCounter++;
             order = new List<Product>();
         }
 
+        // ADD PRODUCT TO ORDER
         public void AddToOrder(Product product)
         {
             order.Add(product);
         }
 
+        // REMOVE PRODUCT FROM ORDER
         public void RemoveFromOrder(Product product)
         {
             order.Remove(product);
         }
 
+        // SHOW A STRING OF ALL PRODUCTS IN THE ORDER CONCATENATED
         public string DisplayOrderProducts()
         {
             string orderProductList = "";
@@ -38,11 +43,13 @@ namespace ACME_Web_App
             return orderProductList;
         }
 
+        // RETURNS THE ACTUAL PRODUCT OBJECTS IN THE ORDER
         public List<Product> GetOrderProducts()
         {
             return order;
         }
 
+        // GETS ORDER ID
         public string GetOrderID()
         {
             return orderID.ToString();
