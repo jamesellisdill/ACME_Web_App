f806571d32dc947737b5239d7e89cce94b0c78df
CoryAllenOfficial <coryallen92@gmail.com>
2024-07-19 11:49:40 -0400

    Added class Order and OrderManager
    These will work with product and inventory. Chat has info on some use cases. In programming chat

---
+++ Order.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACME_Web_App
+{
+    public class Order
+    {
+        private List<Product> order;
+        private static int orderIDCounter = 6000000;
+        private int orderID;
+
+        public Order()
+        {
+            orderID = orderIDCounter++;
+            order = new List<Product>();
+        }
+
+        public void AddToOrder(Product product)
+        {
+            order.Add(product);
+        }
+
+        public void RemoveFromOrder(Product product)
+        {
+            order.Remove(product);
+        }
+
+        public string DisplayOrderProducts()
+        {
+            string orderProductList = "";
+            foreach (Product product in order)
+            {
+                orderProductList += product.ViewProductInfo() + "\n";
+            }
+            return orderProductList;
+        }
+
+        public List<Product> GetOrderProducts()
+        {
+            return order;
+        }
+
+        public string GetOrderID()
+        {
+            return orderID.ToString();
+        }
+    }
+}
---
+++ OrderManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACME_Web_App
+{
+    public class OrderManager
+    {
+        private List<Order> orders;
+
+        public OrderManager()
+        {
+            orders = new List<Order>();
+        }
+
+        public Order CreateNewOrder()
+        {
+            Order newOrder = new Order();
+            orders.Add(newOrder);
+            return newOrder;
+        }
+
+        public void AddToOrder(Order order, Product product)
+        {
+            order.AddToOrder(product);
+        }
+
+        public void RemoveFromOrder(Order order, Product product)
+        {
+            order.RemoveFromOrder(product);
+        }
+
+        public string PlaceOrder(Order order)
+        {
+            return order.GetOrderID();
+        }
+    }
+}
