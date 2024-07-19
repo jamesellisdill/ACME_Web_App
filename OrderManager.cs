b4c6f76fa74dfdc09a79d7c19632278531208703
CoryAllenOfficial <coryallen92@gmail.com>
2024-07-19 14:14:17 -0400

    Updated OrderManager.cs to include comments

--- OrderManager.cs
+++ OrderManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +8,16 @@ namespace ACME_Web_App
 {
     public class OrderManager
     {
+        // CREATED LIST OF ORDERS
         private List<Order> orders;
 
+        // CONSTRUCTOR
         public OrderManager()
         {
             orders = new List<Order>();
         }
 
+        // CREATES A NEW ORDER AND ADDS IT TO 'orders' LIST
         public Order CreateNewOrder()
         {
             Order newOrder = new Order();
@@ -22,16 +25,19 @@ namespace ACME_Web_App
             return newOrder;
         }
 
+        // EDITS THE ORDER PASSED IN BY ADDING IN THE PRODUCT PASSED IN. IT USES THE OBJECTS ADD METHOD TO ADD.
         public void AddToOrder(Order order, Product product)
         {
             order.AddToOrder(product);
         }
 
+        // EDITS THE ORDER PASSED IN BY REMOVING IN THE PRODUCT PASSED IN. IT USES THE OBJECTS REMOVE METHOD TO REMOVE.
         public void RemoveFromOrder(Order order, Product product)
         {
             order.RemoveFromOrder(product);
         }
 
+        // CURRENTLY IT JUST RETURNS THE ORDER ID TO USE AS A CONFIRMATION NUMBER. THIS MAY BE EXPANDED ON LATER AS WE IMPLEMENT THE SOLUTION.
         public string PlaceOrder(Order order)
         {
             return order.GetOrderID();
