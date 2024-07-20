3651beddd1b1beeb3184a386f73662a1572732bb
CoryAllenOfficial <coryallen92@gmail.com>
2024-07-20 16:57:54 -0400

    Added users to main

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +11,11 @@ namespace ACME_Web_App
         public static CustomersDatabase customersDatabase = new CustomersDatabase();
         public static Inventory inventory = new Inventory();
         public static OrderManager orderManager = new OrderManager();
+        public static User salesmanUser = new Salesman("salesman", "ACME", false, true);    // salesman
+        public static User adminUser = new Admin("admin", "ACME", true, true);              // admin
+        public static User inactiveUser = new Salesman("salesman2", "ACME", false, false);   // deactivated account
+
+        public static List<User> users = new List<User> {salesmanUser, adminUser, inactiveUser};
 
         /// <summary>
         /// The main entry point for the application.
