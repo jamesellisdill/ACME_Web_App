7ba5633e2a5bacabee09ebe64644de6222a51284
CoryAllenOfficial <coryallen92@gmail.com>
2024-07-20 15:27:54 -0400

    Updated class access

--- Customer.cs
+++ Customer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace ACME_Web_App
 {
-    internal class Customer
+    public class Customer
     {
         private string name;
         private string address;
